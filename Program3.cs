4dc9a74f20b386808c97b5b105e70b2e795bfd0b
Patymaster <62792625+Patymaster@users.noreply.github.com>
2023-02-23 22:06:35 +0200

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введіть шлях до файлу");
+            string path = Console.ReadLine();
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                // Групування продуктів за ціною
+                var groupedProducts = lines
+                    .Select(line => new {
+                        Name = line.Split(',')[0],
+                        Price = int.Parse(line.Split(',')[1])
+                    })
+                    .GroupBy(product => product.Price);
+
+                // Вивід
+                Console.WriteLine("Grouped products by price:");
+                foreach (var group in groupedProducts)
+                {
+                    Console.WriteLine($"Price: {group.Key}");
+                    foreach (var product in group)
+                    {
+                        Console.WriteLine($"\t{product.Name}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Помилка: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Program4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        delegate int StringLengthDelegate(string str);
+
+        static void Main()
+        {
+            // Заповнення списку рядків
+            List<string> strings = new List<string>();
+            Console.WriteLine("Введіть рядки:");
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str == "")
+                {
+                    break;
+                }
+                strings.Add(str);
+            }
+
+            // Створення делегата за допомогою лямбда-виразу
+            StringLengthDelegate stringLengthDelegate = str => str.Length;
+
+            // Обробка списку рядків за допомогою делегата
+            Console.WriteLine("Довжини рядків:");
+            foreach (var str in strings)
+            {
+                int length = stringLengthDelegate(str);
+                Console.WriteLine($"{str}: {length}");
+            }
+        }
+    }
+}
